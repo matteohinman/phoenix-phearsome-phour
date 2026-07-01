e7b9afb8173a7e89596c5e042e597d561ea0638f
Matteo Hinman <222314594+matteohinman@users.noreply.github.com>
2026-07-01 16:25:03 -0700

    Add Tina Thompson to Uno Fresco leaderboard

--- js/leaderboard-data.js
+++ js/leaderboard-data.js
@@ -9,6 +9,7 @@ const leaderboardEntries = [
   { name: "Sabrina Huelga", tierKey: "uno-fresco", gender: "F", age: "N/S", date: "2019-10-19", time: "3:15:55" },
   { name: "Joe Shea", tierKey: "uno-fresco", gender: "M", age: "N/S", date: "2019-07-27", time: "2:54:57" },
   { name: "Wayne Jung", tierKey: "uno-fresco", gender: "M", age: "N/S", date: "2019-07-27", time: "3:22:49" },
+  { name: "Tina Thompson", tierKey: "uno-fresco", gender: "F", age: "N/S", date: "2019-07-28", time: "4:36:48" },
 ];
 
 const leaderboardTiers = [
