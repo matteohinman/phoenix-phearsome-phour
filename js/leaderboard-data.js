ca44396dc425e6985d73ced5949d3b58a642e6a7
Matteo Hinman <222314594+matteohinman@users.noreply.github.com>
2026-07-01 16:20:13 -0700

    Add Joe Shea to Uno Fresco leaderboard

--- js/leaderboard-data.js
+++ js/leaderboard-data.js
@@ -7,6 +7,7 @@ const leaderboardEntries = [
   { name: "Sean Caudill", tierKey: "uno-fresco", gender: "M", age: "N/A", date: "2020-08-29", time: "2:14:26" },
   { name: "Nicholas Mazur", tierKey: "uno-fresco", gender: "M", age: "N/A", date: "2020-08-29", time: "2:35:48" },
   { name: "Sabrina Huelga", tierKey: "uno-fresco", gender: "F", age: "N/A", date: "2019-10-19", time: "3:15:55" },
+  { name: "Joe Shea", tierKey: "uno-fresco", gender: "M", age: "N/A", date: "2019-07-27", time: "2:54:57" },
 ];
 
 const leaderboardTiers = [
